842d5441ddef9ee666b77ca0e62ffe434458f085
Leo <leo.longlhb@gmail.com>
2024-06-19 19:53:18 +0400

    update

--- src/main/java/org/collection/dialogform/LoginDialog.java
+++ src/main/java/org/collection/dialogform/LoginDialog.java
@@ -1,10 +1,11 @@
 package org.collection.dialogform;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class LoginDialog extends JDialog {
+public class LoginDialog {
     private JButton btnClose;
     private JButton btnLogin;
     private JTextField txtUsername;
@@ -12,8 +13,59 @@ public class LoginDialog extends JDialog {
     private JCheckBox chbRememberMe;
     private JPanel loginPanel;
 
-    private  static JDialog loginDialog = new LoginDialog();
+    private  static JFrame loginDialog = new JFrame();
 
+    public JButton getBtnClose() {
+        return btnClose;
+    }
+
+    public void setBtnClose(JButton btnClose) {
+        this.btnClose = btnClose;
+    }
+
+    public JButton getBtnLogin() {
+        return btnLogin;
+    }
+
+    public void setBtnLogin(JButton btnLogin) {
+        this.btnLogin = btnLogin;
+    }
+
+    public JTextField getTxtUsername() {
+        return txtUsername;
+    }
+
+    public void setTxtUsername(JTextField txtUsername) {
+        this.txtUsername = txtUsername;
+    }
+
+    public JPasswordField getTxtPassword() {
+        return txtPassword;
+    }
+
+    public void setTxtPassword(JPasswordField txtPassword) {
+        this.txtPassword = txtPassword;
+    }
+
+    public JCheckBox getChbRememberMe() {
+        return chbRememberMe;
+    }
+
+    public void setChbRememberMe(JCheckBox chbRememberMe) {
+        this.chbRememberMe = chbRememberMe;
+    }
+
+    public JPanel getLoginPanel() {
+        return loginPanel;
+    }
+
+    public void setLoginPanel(JPanel loginPanel) {
+        this.loginPanel = loginPanel;
+    }
+
+    public static void setLoginDialog(JFrame loginDialog) {
+        LoginDialog.loginDialog = loginDialog;
+    }
 
     public LoginDialog() {
         btnClose.addActionListener(new ActionListener() {
@@ -22,14 +74,28 @@ public class LoginDialog extends JDialog {
                 System.exit(0);
             }
         });
+        btnLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = txtUsername.getText();
+                String password = txtPassword.getText();
+                if(username.equals("admin") && password.equals("123")) {
+                    loginDialog.dispose();
+                }
+                else {
+                    JOptionPane.showMessageDialog(null, "Invalid username or password!", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
     }
 
     public void getLoginDialog() {
-//        loginDialog.getContentPane().add(loginPanel);
-//        loginDialog.pack();
-//        loginDialog.setVisible(true);
-//        loginDialog.setSize(400, 250);
-//        loginDialog.setLocationRelativeTo(null);
+        JDialog frameLogin = new JDialog(loginDialog,"Login", Dialog.ModalityType.APPLICATION_MODAL);
+        frameLogin.getContentPane().add(loginPanel);
+        frameLogin.pack();
+        frameLogin.setVisible(true);
+        frameLogin.setLocationRelativeTo(null);
+        frameLogin.setSize(400,250);
     }
 
 }
