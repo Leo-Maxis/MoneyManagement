24032dea7dbcebd5cbf8b0fbae3a2ec21cc6ca55
Leo-Maximus <leo.longlhb@gmail.com>
2024-06-19 18:18:27 +0700

    update function

--- src/main/java/org/collection/tabs/AddExpenditureTypePanel.java
+++ src/main/java/org/collection/tabs/AddExpenditureTypePanel.java
@@ -5,6 +5,7 @@ import org.collection.entity.ExpenditureType;
 import org.collection.util.MessageBox;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -17,13 +18,35 @@ public class AddExpenditureTypePanel {
     private JButton btnUpdate;
     private JButton btnDelete;
     private JButton btnList;
+    private JButton btnEdit;
 
+
+    private void changeButtonState(boolean edit, boolean save, boolean update, boolean delete) {
+        btnEdit.setEnabled(edit);
+        btnSave.setEnabled(save);
+        btnUpdate.setEnabled(update);
+        btnDelete.setEnabled(delete);
+    }
 
     public AddExpenditureTypePanel() {
+        changeButtonState(false, true, false, false);
         btnSave.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
+                    StringBuilder sb = new StringBuilder();
+                    if (txtName.getText().equals("")) {
+                        sb.append("Name must be entered");
+                        txtName.setBackground(Color.yellow);
+                        txtName.setEditable(true);
+                    }
+                    else {
+                        txtName.setBackground(Color.white);
+                    }
+                    if (!sb.isEmpty()) {
+                        MessageBox.showErrorMessage(null, "Error", sb.toString());
+                        return;
+                    }
                     ExpenditureType entity = new ExpenditureType();
                     entity.setName(txtName.getText());
                     ExpenditureTypeDAO dao = new ExpenditureTypeDAO();
@@ -32,11 +55,26 @@ public class AddExpenditureTypePanel {
                     MessageBox.showInfomationMessage(null, "Infomation","Type is saved");
                     txtID.setEditable(false);
                     txtName.setEditable(false);
+                    changeButtonState(true, false, true, true);
                 } catch (Exception exception) {
 //                    JOptionPane.showMessageDialog(null, "Error: " + exception.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
                     exception.printStackTrace();
                     MessageBox.showErrorMessage(null, "Error", exception.getMessage());
                 }
+            }
+        });
+        btnEdit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtName.setEditable(true);
+            }
+        });
+        btnNew.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtID.setText("");
+                txtName.setText("");
+                changeButtonState(false, true, false, false);
             }
         });
     }
